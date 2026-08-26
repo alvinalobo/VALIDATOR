5ed8ce37742751085d725d4af89d54686d512069
Dasarapu Nihal <84036926+mrblue24390@users.noreply.github.com>
2026-08-26 13:59:43 +0530

    Update main.py

--- rule ingestion/app/main.py
+++ rule ingestion/app/main.py
@@ -1,6 +1,9 @@
+
 from fastapi import FastAPI
 from app.api.connector_routes import router as connector_router
 from app.connector.plugin_loader import load_plugins
+
+from app.api.rules import router as rule_router
 
 app = FastAPI(
     title="Rule Ingestion Service",
@@ -10,6 +13,7 @@ app = FastAPI(
 load_plugins()
 
 app.include_router(connector_router)
+app.include_router(rule_router)
 
 @app.get("/")
 def home():
